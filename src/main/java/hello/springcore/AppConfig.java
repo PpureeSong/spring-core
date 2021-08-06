80c056e4faf42e59d8c21ffcaa4e5d5b18c0260c
Ppuree Song <roo0622@gmail.com>
2021-08-06 23:25:02 +0900

    스프링으로 전환하기

--- src/main/java/hello/springcore/AppConfig.java
+++ src/main/java/hello/springcore/AppConfig.java
@@ -1,34 +1,41 @@
 package hello.springcore;
 
 import hello.springcore.discount.DiscountPolicy;
-import hello.springcore.discount.FixDiscountPolicy;
+import hello.springcore.discount.RateDiscountPolicy;
 import hello.springcore.member.MemberService;
 import hello.springcore.member.MemberServiceImpl;
 import hello.springcore.member.MemoryMemberRepository;
 import hello.springcore.order.OrderService;
 import hello.springcore.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
- * application에 대한 환경설정은 AppConfig에서 다 함
+ * application에 대한 환경설정, 구성정보는 AppConfig에서 다 함
  * AppConfig는 생성한 객체 인스턴스의 참조(레퍼런스)를 "생성자를 통해서 주입(연결)" 해준다.
  * 객체의 생성과 연결 담당
  */
+@Configuration
 public class AppConfig {
 
     // 생성자 주입
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
--- src/main/java/hello/springcore/OrderApp.java
+++ src/main/java/hello/springcore/OrderApp.java
@@ -5,14 +5,21 @@ import hello.springcore.member.Member;
 import hello.springcore.member.MemberService;
 import hello.springcore.order.Order;
 import hello.springcore.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
+//        AppConfig appConfig = new AppConfig();
+//
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
 
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
