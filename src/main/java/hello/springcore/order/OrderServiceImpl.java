b361863f721fd6148dd7d10130861186fb33bf8b
Ppuree Song <roo0622@gmail.com>
2021-08-11 21:24:09 +0900

    의존관계 주입 - 생성자 주입

--- src/main/java/hello/springcore/order/OrderServiceImpl.java
+++ src/main/java/hello/springcore/order/OrderServiceImpl.java
@@ -14,7 +14,16 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    @Autowired // 생성자 위에 애노테이션 설정을 해주면 의존관계를 자동으로 주입
+    //
+
+    /**
+     * 생성자 위에 애노테이션 설정을 해주면 의존관계를 자동으로 주입
+     * 생성자 주입 권장 이유
+     *   - 불변하게 설계할 수 있음
+     *   - 주입 방지 : 주입 데이터를 누락했을 때 컴파일 오류 발생
+     *   - final 키워드를 사용할 수 있음
+     */
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
---
+++ src/test/java/hello/springcore/autowired/AutowiredTest.java
@@ -0,0 +1,42 @@
+package hello.springcore.autowired;
+
+import hello.springcore.member.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.lang.Nullable;
+
+import java.util.Optional;
+
+public class AutowiredTest {
+
+    @Test
+    void AutowiredOption() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+    }
+
+    static class TestBean {
+
+        // 자동 주입할 대상이 없으면 수정자 메서드 자체가 호출 안됨
+        @Autowired(required = false)
+        public void setNoBean1(Member noBean1) {
+            System.out.println("noBean1 = " + noBean1);
+        }
+
+        // 자동 주입할 대상이 없으면, 호출은 되는데 null이 입력됨
+        @Autowired
+        public void setNoBean2(@Nullable Member noBean2) {
+            System.out.println("noBean2 = " + noBean2);
+        }
+
+        // 자동 주입할 대상이 없으면, 호출은 되는데  Optional.empty가 입력됨
+        @Autowired
+        public void setNoBean3(Optional<Member> noBean3) {
+            System.out.println("noBean3 = " + noBean3);
+        }
+        
+        
+
+    }
+}
