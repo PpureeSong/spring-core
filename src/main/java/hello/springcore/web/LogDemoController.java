0ad3e2f74d82efff3cb4ff426a8a2293e1038f1b
Ppuree Song <roo0622@gmail.com>
2021-08-13 22:28:04 +0900

    웹 스코프와 Provider 적용

---
+++ src/main/java/hello/springcore/web/LogDemoController.java
@@ -0,0 +1,34 @@
+package hello.springcore.web;
+
+import hello.springcore.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    // ObjectProvider : 호출하는 시점까지 request scope 빈의 생성을 지연 할 수 있음
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURL().toString();
+
+        MyLogger myLogger = myLoggerProvider.getObject();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+
+        return "OK";
+    }
+
+}
